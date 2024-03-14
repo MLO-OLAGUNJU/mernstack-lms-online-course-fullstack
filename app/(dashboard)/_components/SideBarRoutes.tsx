065c103bed005d4d47e33c310c1901c4111c8ea7
OLAGUNJU Oladele Emmanuel <149441709+MLO-OLAGUNJU@users.noreply.github.com>
2024-03-14 03:30:46 +0100

    fullstack mern lms online course website

--- app/(dashboard)/_components/SideBarRoutes.tsx
+++ app/(dashboard)/_components/SideBarRoutes.tsx
@@ -1,8 +1,9 @@
 "use client";
 
-import { Compass, Layout } from "lucide-react";
+import { BarChart, Compass, Layout, List } from "lucide-react";
 import React from "react";
 import SideBarItem from "./SideBarItem";
+import { usePathname } from "next/navigation";
 
 const guestRoutes = [
   {
@@ -17,8 +18,25 @@ const guestRoutes = [
   },
 ];
 
+const teacherRoutes = [
+  {
+    icon: List,
+    label: "Courses",
+    href: "/teacher/courses",
+  },
+  {
+    icon: BarChart,
+    label: "Analytics",
+    href: "/teacher/analytics",
+  },
+];
+
 const SideBarRoutes = () => {
-  const routes = guestRoutes;
+  const pathname = usePathname();
+  const isTeacherPage = pathname?.includes("/teacher");
+
+  const routes = isTeacherPage ? teacherRoutes : guestRoutes;
+
   return (
     <div className="flex flex-col w-full">
       {routes.map((route) => (
