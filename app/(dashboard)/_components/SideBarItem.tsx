1e661a85dbb5c5551ac04dd2579c0d24d7bb65ea
OLAGUNJU Oladele Emmanuel <149441709+MLO-OLAGUNJU@users.noreply.github.com>
2024-03-14 01:44:27 +0100

    fullstack mern lms online course website

--- app/(dashboard)/_components/SideBarItem.tsx
+++ app/(dashboard)/_components/SideBarItem.tsx
@@ -23,25 +23,30 @@ const SideBarItem = ({ icon: Icon, label, href }: SideBarItemProps) => {
   };
 
   return (
-    <div>
-      <button
-        onClick={onClick}
-        type="button"
-        className={cn(
-          "flex items-center gap-x-2 text-slate-500 text-sm font-[500] w-full pl-6 transition-all hover:text-slate-600 hover:bg-slate-300/20",
-          isActive &&
-            "text-sky-700 bg-sky-200/20 hover:bg-sky-200/20 hover:text-sky-700"
-        )}
-      >
-        <div className="flec items-center gap-x-2 py-4">
-          <Icon
-            size={22}
-            className={cn("text-slate-500", isActive && "text-[#3857A1]")}
-          />
-        </div>
+    <button
+      onClick={onClick}
+      type="button"
+      className={cn(
+        "flex items-center gap-x-2 text-slate-500 text-sm font-[500] w-full pl-6 transition-all hover:text-slate-600 hover:bg-slate-300/20",
+        isActive &&
+          "text-[#3857A1] bg-sky-200/20 hover:bg-sky-200/20 hover:text-[#3857A1]"
+      )}
+    >
+      <div className="flex items-center gap-x-2 py-4">
+        <Icon
+          size={22}
+          className={cn("text-slate-500", isActive && "text-[#3857A1]")}
+        />
         {label}
-      </button>
-    </div>
+      </div>
+
+      <div
+        className={cn(
+          "ml-auto opacity-0 border-2 border-solid border-red h-full transition-all",
+          isActive && "opacity-100"
+        )}
+      />
+    </button>
   );
 };
 
