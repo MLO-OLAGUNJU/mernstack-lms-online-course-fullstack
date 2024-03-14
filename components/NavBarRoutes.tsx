10490c3dbe73f06964760fb9cfded0ecb1de5678
OLAGUNJU Oladele Emmanuel <149441709+MLO-OLAGUNJU@users.noreply.github.com>
2024-03-14 02:44:08 +0100

    fullstack mern lms online course website

--- components/NavBarRoutes.tsx
+++ components/NavBarRoutes.tsx
@@ -1,12 +1,37 @@
 "use client";
 
 import { UserButton } from "@clerk/nextjs";
+import { usePathname, useRouter } from "next/navigation";
 import React from "react";
+import { Button } from "./ui/button";
+import { LogOut } from "lucide-react";
+import Link from "next/link";
 
 const NavBarRoutes = () => {
+  const pathname = usePathname();
+  const router = useRouter();
+
+  const isTeacherPage = pathname?.startsWith("/teacher");
+  const isPlayerPage = pathname?.includes("/chapter");
+
   return (
-    <div className="flex gap-x-2 ml-auto">
-      <UserButton />
+    <div className="flex items-center gap-x-5 ml-auto">
+      {isTeacherPage || isPlayerPage ? (
+        <Button size="sm" variant="ghost">
+          <LogOut className="h-4 w-4 mr-2" />
+          Exit
+        </Button>
+      ) : (
+        <Link
+          href={"/teacher/courses"}
+          className="border-[#3857A1] rounded-md border-1 bg-white border-solid"
+        >
+          <Button size="sm" variant="ghost">
+            Teacher mode
+          </Button>
+        </Link>
+      )}
+      <UserButton afterSignOutUrl="/" />
     </div>
   );
 };
