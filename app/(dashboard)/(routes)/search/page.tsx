d7f94c1701957965c386b0426d556744397558ce
OLAGUNJU Oladele Emmanuel <149441709+MLO-OLAGUNJU@users.noreply.github.com>
2024-03-14 02:08:01 +0100

    fullstack mern lms online course website

---
+++ app/(dashboard)/(routes)/search/page.tsx
@@ -0,0 +1,9 @@
+"use client";
+
+import React from "react";
+
+const Searchpage = () => {
+  return <div>Searchpage</div>;
+};
+
+export default Searchpage;
--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -3,11 +3,11 @@ import SideBar from "./_components/SideBar";
 
 const DashboardLayout = ({ children }: { children: React.ReactNode }) => {
   return (
-    <div className=" h-full">
+    <div className=" h-full select-none">
       <div className=" hidden md:flex h-full w-56 flex-col fixed inset-y-0 z-50">
         <SideBar />
       </div>
-      {children}
+      <main className="md:pl-56 h-full">{children}</main>
     </div>
   );
 };
