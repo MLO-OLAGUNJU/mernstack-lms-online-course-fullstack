39985f9de9d78b59f708722e1a6fb0eeccfc2bf8
OLAGUNJU Oladele Emmanuel <149441709+MLO-OLAGUNJU@users.noreply.github.com>
2024-03-15 01:26:05 +0100

    fullstack mern lms online course website

---
+++ components/providers/toaster-provider.tsx
@@ -0,0 +1,14 @@
+"use client";
+
+import React from "react";
+import { Toaster } from "react-hot-toast";
+
+const ToasterProvider = () => {
+  return (
+    <div>
+      <Toaster />
+    </div>
+  );
+};
+
+export default ToasterProvider;
