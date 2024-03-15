3d7307fe3777f254e3681b2cf66106db06573dc2
OLAGUNJU Oladele Emmanuel <149441709+MLO-OLAGUNJU@users.noreply.github.com>
2024-03-15 01:11:21 +0100

    fullstack mern lms online course website

--- app/(dashboard)/(routes)/teacher/courses/page.tsx
+++ app/(dashboard)/(routes)/teacher/courses/page.tsx
@@ -1,9 +1,17 @@
 "use client";
 
+import { Button } from "@/components/ui/button";
 import React from "react";
+import Link from "next/link";
 
-const page = () => {
-  return <div>page</div>;
+const CoursesPage = () => {
+  return (
+    <div className="p-6">
+      <Link href="/teacher/create">
+        <Button>New Course</Button>
+      </Link>
+    </div>
+  );
 };
 
-export default page;
+export default CoursesPage;
