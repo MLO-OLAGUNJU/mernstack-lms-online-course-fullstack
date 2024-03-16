ebe54a0f5b1cc0a667b8729e25b7662802251c25
OLAGUNJU Oladele Emmanuel <149441709+MLO-OLAGUNJU@users.noreply.github.com>
2024-03-16 14:50:03 +0100

    fullstack mern lms online course website

--- app/(dashboard)/(routes)/teacher/courses/[courseId]/page.tsx
+++ app/(dashboard)/(routes)/teacher/courses/[courseId]/page.tsx
@@ -30,16 +30,24 @@ const CourseIdPage = async ({ params }: { params: { courseId: string } }) => {
   const totalFields = requiredFields.length;
   const completedFields = requiredFields.filter(Boolean).length;
 
-  const completionText = `(${completedFields} / ${totalFields})`;
+  const completionText = `(${completedFields}/${totalFields})`;
 
   return (
     <div className="p-6">
       <div className="flex items-center justify-between">
         <div className=" flex flex-col gap-y-2">
           <h1 className="text-2xl font-bold">Course setup</h1>
-          <span>
+          <span className="text-sm text-slate-700">
             Complete all fields <span>{completionText}</span>
           </span>
+        </div>
+      </div>
+
+      <div className="grid grid-cols-1 md:grid-cols-2 gap-6 mt-16">
+        <div>
+          <div className="flex items-center gap-x-2">
+            <h2 className="text-xl">Customize your course</h2>
+          </div>
         </div>
       </div>
     </div>
---
+++ components/IconBadge.tsx
@@ -0,0 +1,3 @@
+import { LucideIcon } from "lucide-react";
+import { cva, type VariantProps } from "class-variance-authority";
+import { cn } from "@/lib/utils";
