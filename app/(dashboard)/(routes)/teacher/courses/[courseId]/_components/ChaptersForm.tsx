51023cc1bb42da88ede9128065faf02fc84f35f0
OLAGUNJU Oladele Emmanuel <149441709+MLO-OLAGUNJU@users.noreply.github.com>
2024-03-23 19:03:11 +0100

    fullstack mern lms online course website

---
+++ app/(dashboard)/(routes)/teacher/courses/[courseId]/_components/ChaptersForm.tsx
@@ -0,0 +1,121 @@
+"use client";
+import React, { useState } from "react";
+import { Course } from "@prisma/client";
+import * as z from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormMessage,
+} from "@/components/ui/form";
+import { Button } from "@/components/ui/button";
+import { Pencil } from "lucide-react";
+import toast from "react-hot-toast";
+import axios from "axios";
+import { useRouter } from "next/navigation";
+import { cn } from "@/lib/utils";
+import { Textarea } from "@/components/ui/textarea";
+
+interface ChaptersFormProps {
+  initialData: Course;
+  courseId: string;
+}
+
+const formSchema = z.object({
+  description: z.string().min(1, {
+    message: "Description is required",
+  }),
+});
+
+const ChaptersForm = ({ initialData, courseId }: ChaptersFormProps) => {
+  const [isEditing, setIsEditing] = useState(false);
+
+  const router = useRouter();
+
+  const toggleEdit = () => {
+    setIsEditing((current) => !current);
+  };
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: { description: initialData?.description || "" },
+  });
+
+  const { isSubmitting, isValid } = form.formState;
+
+  const onSubmit = async (values: z.infer<typeof formSchema>) => {
+    try {
+      await axios.patch(`/api/courses/${courseId}`, values);
+      toast.success("Course updated successfully");
+      toggleEdit();
+      router.refresh();
+    } catch (error) {
+      toast.error("Something went wrong");
+    }
+  };
+
+  return (
+    <div className="mt-6 border bg-[#d0deff] rounded-md p-4">
+      <div className=" font-medium flex items-center justify-between">
+        Course description
+        <Button onClick={toggleEdit} variant={"ghost"} className="bg-white">
+          {isEditing ? (
+            <>Cancel</>
+          ) : (
+            <>
+              <Pencil className="h-4 w-4 mr-2" />
+              <span className="md:hidden hidden lg:flex">Edit description</span>
+            </>
+          )}
+        </Button>
+      </div>
+      {!isEditing && (
+        <p
+          className={cn(
+            "text-sm mt-2",
+            !initialData.description && "text-slate-600 italic"
+          )}
+        >
+          {initialData.description || "No description"}
+        </p>
+      )}
+      {isEditing && (
+        <>
+          <Form {...form}>
+            <form
+              onSubmit={form.handleSubmit(onSubmit)}
+              className="space-y-4 mt-4"
+            >
+              <FormField
+                control={form.control}
+                name="description"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormControl>
+                      <Textarea
+                        disabled={isSubmitting}
+                        placeholder="e.g. This comprehensive course equips you with the essential skills and knowledge to kickstart your software engineering career...."
+                        {...field}
+                      />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <div className="flex items-center gap-x-2">
+                <Button disabled={!isValid || isSubmitting} type="submit">
+                  Save
+                </Button>
+              </div>
+            </form>
+          </Form>
+        </>
+      )}
+    </div>
+  );
+};
+
+export default ChaptersForm;
--- app/(dashboard)/(routes)/teacher/courses/[courseId]/page.tsx
+++ app/(dashboard)/(routes)/teacher/courses/[courseId]/page.tsx
@@ -15,6 +15,7 @@ import ImageForm from "./_components/ImageForm";
 import { CategoryForm } from "./_components/CategoryForm";
 import PriceForm from "./_components/PriceForm";
 import AttachemntsForm from "./_components/AttachemntsForm";
+import ChaptersForm from "./_components/ChaptersForm";
 
 const CourseIdPage = async ({ params }: { params: { courseId: string } }) => {
   const { userId } = auth();
@@ -22,11 +23,18 @@ const CourseIdPage = async ({ params }: { params: { courseId: string } }) => {
   if (!userId) {
     return redirect("/");
   }
+
   const course = await db.course.findUnique({
     where: {
       id: params.courseId,
+      userId,
     },
     include: {
+      chapters: {
+        orderBy: {
+          position: "asc",
+        },
+      },
       attachments: {
         orderBy: {
           createdAt: "desc",
@@ -53,6 +61,7 @@ const CourseIdPage = async ({ params }: { params: { courseId: string } }) => {
     course.imageUrl,
     course.price,
     course.categoryId,
+    course.chapters.some((chapter) => chapter.isPublished),
   ];
 
   const totalFields = requiredFields.length;
@@ -105,7 +114,7 @@ const CourseIdPage = async ({ params }: { params: { courseId: string } }) => {
               />
               <h2 className="text-xl">Course chapters</h2>
             </div>
-            <div>TODO: Chapters</div>
+            <ChaptersForm initialData={course} courseId={course.id} />
           </div>
           <div className="flex items-center gap-x-2">
             <IconBadges
