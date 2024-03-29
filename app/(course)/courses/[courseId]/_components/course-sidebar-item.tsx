6d24b8bb0e58ef2ea5a199bf63a7ab41e984b968
OLAGUNJU Oladele Emmanuel <149441709+MLO-OLAGUNJU@users.noreply.github.com>
2024-03-29 16:17:45 +0100

    fullstack mern lms online course website

--- app/(course)/courses/[courseId]/_components/course-sidebar-item.tsx
+++ app/(course)/courses/[courseId]/_components/course-sidebar-item.tsx
@@ -35,9 +35,9 @@ export const CourseSidebarItem = ({
       onClick={onClick}
       type="button"
       className={cn(
-        "flex items-center gap-x-2 text-slate-500 text-sm font-[500] pl-6 transition-all hover:text-slate-600 hover:bg-slate-300/20",
+        "flex items-center gap-x-2 text-slate-500 text-sm font-[500] w-full pl-6 transition-all hover:text-slate-600 hover:bg-slate-300/20",
         isActive &&
-          "text-slate-700 bg-slate-200/20 hover:bg-slate-200/20 hover:text-slate-700",
+          "text-[#3857A1] bg-[#d0deff] hover:bg-[#d0deff] hover:text-[#3857A1]",
         isCompleted && "text-emerald-700 hover:text-emerald-700",
         isCompleted && isActive && "bg-emerald-200/20"
       )}
@@ -47,7 +47,7 @@ export const CourseSidebarItem = ({
           size={22}
           className={cn(
             "text-slate-500",
-            isActive && "text-slate-700",
+            isActive && "text-[#3857A1]",
             isCompleted && "text-emerald-700"
           )}
         />
@@ -55,7 +55,7 @@ export const CourseSidebarItem = ({
       </div>
       <div
         className={cn(
-          "ml-auto opacity-0 border-2 border-slate-700 h-full transition-all",
+          "ml-auto opacity-0 border-2 border-solid border-[#3857A1] h-full transition-all",
           isActive && "opacity-100",
           isCompleted && "border-emerald-700"
         )}
