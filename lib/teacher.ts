bf6f15a8596873ae76c9c879757f704dd198f977
OLAGUNJU Oladele Emmanuel <149441709+MLO-OLAGUNJU@users.noreply.github.com>
2024-03-31 01:06:49 +0100

    fullstack mern lms online course website

--- lib/teacher.ts
+++ lib/teacher.ts
@@ -1,3 +1,8 @@
+// export const isTeacher = (userId?: string | null) => {
+//   return userId === process.env.NEXT_PUBLIC_TEACHER_ID;
+// };
+
 export const isTeacher = (userId?: string | null) => {
-  return userId === process.env.NEXT_PUBLIC_TEACHER_ID;
-}
+  const teacherIds = process.env.NEXT_PUBLIC_TEACHER_ID?.split(",") || [];
+  return userId !== undefined && userId !== null && teacherIds.includes(userId);
+};
